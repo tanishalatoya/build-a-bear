fb77641bae378acfae13135f821b1bc7da76b52e
Erin Campbell <iheartmnight@gmail.com>
2019-12-16 17:37:30 -0700

    Add Outfit class and removeGarment function

--- outfit.js
+++ outfit.js
@@ -1,12 +1,21 @@
 class Outfit {
-  constructor() {
+  constructor(title, background, id) {
     this.title = title;
     this.garments = [];
-    this.background = background;
-    this.id = id;
+    this.background = background || 'none';
+    this.id = id || 'anything';
   }
 
   addGarment(attire) {
     this.garments.push(attire)
+  }
+
+  removeGarment() {
+    // itterate over the array with a for loop and find the index of the id of the item we are trying to remove. And then remove it.
+    for (var i = 0; i < this.garments.length; i++) {
+      if(this.garments[i] === this.id) {
+        this.garments.splice(i, 1);
+      }
+    }
   }
 }
